7d1e700c2ae9cfb13150a87267f046f8a06979c3
theTallGuy23 <tinbisrat1923@gmail.com>
2026-01-22 21:06:06 +0300

    Created A Java Project

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+* Name: Tinsae Bisrat
+* Student_ID: 004012464
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
