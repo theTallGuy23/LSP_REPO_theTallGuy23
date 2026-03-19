01429c925629f56452f889fc459c8b4f68215f25
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:40:23 -0400

    Upload code for regular discount processing.

--- src/org/howard/edu/lsp/midterm/strategy/RegularDiscount.java
+++ src/org/howard/edu/lsp/midterm/strategy/RegularDiscount.java
@@ -1,1 +1,18 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * No discount applied for regular customers.
+ */
+public class RegularDiscount implements DiscountStrategy {
+
+    /**
+     * Returns the original price without any discount.
+     *
+     * @param price the original price
+     * @return the same price
+     */
+    @Override
+    public double applyDiscount(double price) {
+        return price;
+    }
+}
