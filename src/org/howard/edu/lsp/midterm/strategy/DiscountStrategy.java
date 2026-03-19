3851e574f7981b07f5f880549182f4c828ec4764
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:38:52 -0400

    Upload code for the interface.
    This interface is going to be the interface that is going to be implemented by many other classes by overriding the method that exists in it, which is the applyDiscount method.

--- src/org/howard/edu/lsp/midterm/strategy/DiscountStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/DiscountStrategy.java
@@ -1,1 +1,15 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * Strategy interface for applying different discount behaviors.
+ */
+public interface DiscountStrategy {
+
+    /**
+     * Applies a discount to the given price.
+     *
+     * @param price the original price
+     * @return the discounted price
+     */
+    double applyDiscount(double price);
+}
