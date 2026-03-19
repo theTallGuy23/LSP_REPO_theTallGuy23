1120b07c16cebf1f8e1b5549be8b115deaf63d9c
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:46:18 -0400

    Write code that gives 15% discount for holidays.

--- src/org/howard/edu/lsp/midterm/strategy/HolidayDiscount.java
+++ src/org/howard/edu/lsp/midterm/strategy/HolidayDiscount.java
@@ -1,1 +1,18 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * Applies a 15% discount for holiday promotions.
+ */
+public class HolidayDiscount implements DiscountStrategy {
+
+    /**
+     * Applies a 15% discount.
+     *
+     * @param price the original price
+     * @return the discounted price
+     */
+    @Override
+    public double applyDiscount(double price) {
+        return price * 0.85;
+    }
+}
