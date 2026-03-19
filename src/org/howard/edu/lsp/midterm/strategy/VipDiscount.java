569577b8bcba435692be6638fba347bddc005785
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:44:17 -0400

    Write code that gives 20% discount for VIP members

--- src/org/howard/edu/lsp/midterm/strategy/VipDiscount.java
+++ src/org/howard/edu/lsp/midterm/strategy/VipDiscount.java
@@ -1,1 +1,18 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * Applies a 20% discount for VIP customers.
+ */
+public class VipDiscount implements DiscountStrategy {
+
+    /**
+     * Applies a 20% discount.
+     *
+     * @param price the original price
+     * @return the discounted price
+     */
+    @Override
+    public double applyDiscount(double price) {
+        return price * 0.80;
+    }
+}
