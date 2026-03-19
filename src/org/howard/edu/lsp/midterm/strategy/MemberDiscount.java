42e729aa05a91e00887a69f686f8a14a915d55c9
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:42:32 -0400

    Write code that gives 10% discount for members.

--- src/org/howard/edu/lsp/midterm/strategy/MemberDiscount.java
+++ src/org/howard/edu/lsp/midterm/strategy/MemberDiscount.java
@@ -1,1 +1,18 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * Applies a 10% discount for members.
+ */
+public class MemberDiscount implements DiscountStrategy {
+
+    /**
+     * Applies a 10% discount.
+     *
+     * @param price the original price
+     * @return the discounted price
+     */
+    @Override
+    public double applyDiscount(double price) {
+        return price * 0.90;
+    }
+}
