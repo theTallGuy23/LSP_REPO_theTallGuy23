550913352e72be59c81aa3daa30396a2c7abe04f
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:48:19 -0400

    Refactored the code to fit the strategy pattern implementation.

--- src/org/howard/edu/lsp/midterm/strategy/PriceCalculator.java
+++ src/org/howard/edu/lsp/midterm/strategy/PriceCalculator.java
@@ -1,1 +1,31 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * PriceCalculator uses a DiscountStrategy to calculate final prices.
+ */
+public class PriceCalculator {
+
+    private DiscountStrategy discountStrategy;
+
+    /**
+     * Sets the discount strategy.
+     *
+     * @param discountStrategy the strategy to apply
+     */
+    public void setDiscountStrategy(DiscountStrategy discountStrategy) {
+        this.discountStrategy = discountStrategy;
+    }
+
+    /**
+     * Calculates the final price using the selected discount strategy.
+     *
+     * @param price the original price
+     * @return the final price after discount
+     */
+    public double calculatePrice(double price) {
+        if (discountStrategy == null) {
+            throw new IllegalStateException("Discount strategy not set.");
+        }
+        return discountStrategy.applyDiscount(price);
+    }
+}
