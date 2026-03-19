2b472890c335bd01610c0fa2d8e4cd89305290ce
theTallGuy23 <tinbisrat1923@gmail.com>
2026-03-19 19:50:04 -0400

    Upload code for the factory.

--- src/org/howard/edu/lsp/midterm/strategy/DiscountStrategyFactory.java
+++ src/org/howard/edu/lsp/midterm/strategy/DiscountStrategyFactory.java
@@ -1,1 +1,27 @@
+package org.howard.edu.lsp.midterm.strategy;
 
+/**
+ * Factory class to create DiscountStrategy objects based on customer type.
+ */
+public class DiscountStrategyFactory {
+
+    /**
+     * Returns the appropriate DiscountStrategy based on customer type.
+     *
+     * @param customerType the type of customer
+     * @return the corresponding DiscountStrategy
+     */
+    public static DiscountStrategy getStrategy(String customerType) {
+        switch (customerType.toUpperCase()) {
+            case "MEMBER":
+                return new MemberDiscount();
+            case "VIP":
+                return new VipDiscount();
+            case "HOLIDAY":
+                return new HolidayDiscount();
+            case "REGULAR":
+            default:
+                return new RegularDiscount();
+        }
+    }
+}
