200945886d5bb8c0c0b4e38fc73d1b18b0cbaa65
theTallGuy23 <tinbisrat1923@gmail.com>
2026-04-23 19:35:59 -0400

    Write code for Report.java

--- src/org/howard/edu/lsp/finalexam/question2/Report.java
+++ src/org/howard/edu/lsp/finalexam/question2/Report.java
@@ -1,1 +1,53 @@
+package org.howard.edu.lsp.finalexam.question2;
 
+/**
+ * Abstract base class for all reports.
+ * Uses the Template Method pattern to define a fixed report-generation workflow.
+ */
+public abstract class Report {
+
+    /**
+     * Template method that defines the fixed workflow for generating a report.
+     */
+    public final void generateReport() {
+        loadData();
+
+        System.out.println("=== HEADER ===");
+        System.out.println(formatHeader());
+        System.out.println();
+
+        System.out.println("=== BODY ===");
+        System.out.println(formatBody());
+        System.out.println();
+
+        System.out.println("=== FOOTER ===");
+        System.out.println(formatFooter());
+        System.out.println();
+    }
+
+    /**
+     * Loads the data needed for the report.
+     */
+    protected abstract void loadData();
+
+    /**
+     * Formats the report header.
+     *
+     * @return the formatted header
+     */
+    protected abstract String formatHeader();
+
+    /**
+     * Formats the report body.
+     *
+     * @return the formatted body
+     */
+    protected abstract String formatBody();
+
+    /**
+     * Formats the report footer.
+     *
+     * @return the formatted footer
+     */
+    protected abstract String formatFooter();
+}
